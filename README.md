ce3f2b5b8b9b26f6504ad5608fd2d3ee5ccd600f
Christofer Jungberg <kiffen_@hotmail.com>
2017-11-02 12:14:12 +0100

    changes layout and set up travis CI

--- README.md
+++ README.md
@@ -2,5 +2,9 @@ Ramverk 2
 ---------------
 
 [![BCH compliance](https://bettercodehub.com/edge/badge/Barelydead/framework2?branch=master)](https://bettercodehub.com/)
+[![Maintainability](https://api.codeclimate.com/v1/badges/2d247096c0bfb7a88f4c/maintainability)](https://codeclimate.com/github/Barelydead/framework2/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/2d247096c0bfb7a88f4c/test_coverage)](https://codeclimate.com/github/Barelydead/framework2/test_coverage)
+[![Build Status](https://travis-ci.org/Barelydead/framework2.svg?branch=master)](https://travis-ci.org/Barelydead/framework2)
+
 
 Detta repo är en del av kursen ramverk2.
--- routes/standard.js
+++ routes/standard.js
@@ -2,28 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('', {
-         title: 'Home',
-         path: 'home'
-     });
+        title: 'Ramverk 2',
+        path: 'home'
+    });
 });
 
 /* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', {
-       title: 'about',
-       path: 'about'
-   });
+router.get('/about', function(req, res) {
+    res.render('about', {
+        title: 'about',
+        path: 'about'
+    });
 });
 
 
 /* GET about page. */
-router.get('/presentation', function(req, res, next) {
-  res.render('presentations', {
-       title: 'presentation',
-       path: 'presentation'
-   });
+router.get('/presentation', function(req, res) {
+    res.render('presentations', {
+        title: 'presentation',
+        path: 'presentation'
+    });
 });
 
 module.exports = router;
